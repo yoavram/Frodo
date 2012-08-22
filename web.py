1a8dd7156147d718ce6b1d748bacd2f1baa1f894
Yoav Ram <yoavram+github@gmail.com>
2012-08-22 15:57:45 +0300

    looks stable. created login page, but it is not functional yet. some of the buttons work (refresh, pause, play).

--- web.py
+++ web.py
@@ -33,12 +33,13 @@ def root_q(jobID = None):
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
+    # http://packages.python.org/Flask-Login/#flaskext.login.LoginManager
+    #if request.method == 'POST':
+        #if valid_login(request.form['username'],
+        #               request.form['password']):
+        #    return log_the_user_in(request.form['username'])
+        #else:
+        #    error = 'Invalid username/password'
     # the code below this is executed if the request method
     # was GET or the credentials were invalid
     return render_template('login.html', error=error)
