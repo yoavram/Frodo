d19ba2ec4067296c07685e510a04df663070f752
Yoav Ram <yoavram+github@gmail.com>
2013-02-07 19:24:30 +0200

    script to create hosts file

---
+++ create_hosts_file.py
@@ -0,0 +1,20 @@
+import paramiko
+import common
+import getpass
+import os
+
+cfg = common.configuration()
+host = cfg.get('sge','host')
+port = cfg.getint('sge','port')
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+username = str(raw_input("Username:"))
+print username
+password = getpass.getpass()
+ssh.connect(host, port, username, password)
+ssh.save_host_keys("hosts")
+if os.path.exists('hosts'):
+	with open('hosts') as f:
+		print f.read()
+else:
+	print "Failed"
